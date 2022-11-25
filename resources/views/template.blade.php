29c57fe6ab5c20ad7f2bcbda9777b42826af3f4d
urielduana <81336230+urielduana@users.noreply.github.com>
2022-11-25 12:14:08 -0600

    NavBar updated

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -13,23 +13,43 @@
 </head>
 
 <body>
+
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="main">Libreria</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand" href="{{ route('home') }}">Libreria Pro</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false"
+                aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
+            <div class="collapse navbar-collapse" id="navbarNavDarkDropdown">
                 <ul class="navbar-nav">
                     <li class="nav-item">
-                        <a class="nav-link" href="main">Home</a>
+                        <a class="nav-link" href="{{ route('home') }}">Home</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="register">Register</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Books
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+{{-- 
+                            <li><a class="dropdown-item" href="{{ route('book.index') }}">Check</a></li>
+                            <li><a class="dropdown-item" href="{{ route('book.create') }}">Register</a></li> --}}
+
+                        </ul>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="customer">Customers</a>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDarkDropdownMenuLink"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Customers
+                        </a>
+                        <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDarkDropdownMenuLink">
+
+                            {{-- <li><a class="dropdown-item" href="{{ route('customer.index') }}">Check</a></li>
+                            <li><a class="dropdown-item" href="{{ route('customer.create') }}">Register</a></li> --}}
+
+                        </ul>
                     </li>
                 </ul>
             </div>
@@ -46,9 +66,9 @@
     </div>
 
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-        </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,20 @@ use App\Http\Controllers\ControladorLibreria;
 Route::get('/', function () {
     return view('main');
 });
+//Home Route
+Route::get('main', [ControladorLibreria::class, 'GetMain'])->name('home');
 
-Route::get('main', [ControladorLibreria::class, 'GetMain']);
+
+
+
+
+
+
+
+
+
+
 Route::get('register', [ControladorLibreria::class, 'GetRegister'])->name('r');
 Route::post('registerBookFilter', [ControladorLibreria::class, 'RegisterBookController']);
-
-
 Route::get('customer', [ControladorLibreria::class, 'GetCustomer'])->name('cus');
-
 Route::post('registerCustomerFilter', [ControladorLibreria::class, 'RegisterCustomerController']);
